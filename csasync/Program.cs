b6af9ee8b648784470331d2957278241ffd08fa9
Nabeel Sulieman <nabsul@outlook.com>
2019-08-08 08:34:30 -0700

    Wrap up example code

--- csasync/Program.cs
+++ csasync/Program.cs
@@ -1,51 +1,64 @@
 using System;
 using System.Diagnostics;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Timers;
 
 namespace csasync
 {
-  
-    class Program
+
+  class Program
     {
-        private async Task WaitAsync(int ms)
+        private Task WaitAsync(int ms)
         {
-            await Task.Delay(ms);
+            return Task.Delay(ms);
         }
 
-        private Task Wait2Async(int ms)
+        public Task Wait2Async(int ms)
         {
-            var interval = TimeSpan.FromMilliseconds(ms);
-            var start = DateTime.Now;
-            while (DateTime.Now - start < interval);
-            return Task.FromResult(true);
+            return Task.Run(() => Thread.Sleep(ms));
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             var t = new Stopwatch();
             t.Start();
-            new Program().Run2().Wait();
+            var prog = new Program();
+            switch(args[0])
+            {
+                case "1":
+                    prog.Run1().Wait();
+                    break;
+                case "2":
+                    prog.Run2().Wait();
+                    break;
+                default:
+                    Console.WriteLine($"Unknown command: {args[0]}");
+                    return;
+            }
             t.Stop();
             Console.WriteLine($"Run time: {t.ElapsedMilliseconds}");
         }
 
         public async Task Run1() 
         {
-            for (int i = 0; i < 10; i++)
-                await Wait2Async(100);
+            for (int i = 0; i < 100; i++)
+            {
+                await WaitAsync(100);
+            }
         }
 
         public async Task Run2()
         {
-            var tasks = new Task[10];
-            for (int i = 0; i < 10; i++)
+            var tasks = new Task[100];
+            for (int i = 0; i < 100; i++)
             {
-                tasks[i] = Wait2Async(100);
+                tasks[i] = WaitAsync(100);
             }
-            await Task.WhenAll(tasks);
+
+            for (int i = 0; i < 100; i++)
+            {
+                await tasks[i];
+            }
         }
     }
 }
